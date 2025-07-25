70fe006945772c5c442f02e7a840c87b193009f9
kunal lokhande <kunallokhande3969@gmail.com>
2025-07-25 14:17:14 -0900

    first commit

--- Back-end/app.js
+++ Back-end/app.js
@@ -34,7 +34,7 @@ app.use(
   session({
     resave: true,
     saveUninitialized: true,
-    secret: process.env.EXPRESS_SESSION_SECRET || "defaultsecret",
+    secret: process.env.EXPRESS_SESSION_SECRETE || "defaultsecret",
   })
 );
 
